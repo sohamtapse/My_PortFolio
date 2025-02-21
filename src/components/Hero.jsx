6d2a881885044fac3a30dcb8fb7df6e725983994
sohamtapse <tapsesoham0@gmail.com>
2025-02-21 20:35:49 +0530

    small css changes done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915EFF]">Soham</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I web devloper.
+            I am FullStack web devloper.
             <br className="sm:block hidden" />
             Crafting AI Solutions using web interface
           </p>
